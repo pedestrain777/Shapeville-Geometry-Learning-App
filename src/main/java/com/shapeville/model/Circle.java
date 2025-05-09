008ca80d177092aa66b9c5468ecb97c9eebe1d91
tddot123 <zjy13121285588@163.com>
2025-05-09 21:23:01 +0800

    支持半径和直径构造圆

--- src/main/java/com/shapeville/model/Circle.java
+++ src/main/java/com/shapeville/model/Circle.java
@@ -5,30 +5,57 @@ import javafx.scene.paint.Color;
 
 public class Circle extends Shape2D {
     private double radius;
-
+    private double inputValue;
+    private boolean inputIsDiameter;
     public Circle(double radius) {
-        super("circle", Color.RED, radius * 2, radius * 2);
-        this.radius = radius;
+        this(radius, false);
     }
-
+    public Circle(double value, boolean isDiameter) {
+        super("circle", Color.RED, (isDiameter ? value/2 : value) * 2, (isDiameter ? value/2 : value) * 2);
+        this.inputIsDiameter = isDiameter;
+        this.inputValue = value;
+        this.radius = isDiameter ? value / 2 : value;
+    }
     @Override
     public double calculateArea() {
         return Math.PI * radius * radius;
     }
-
     public double calculateCircumference() {
         return 2 * Math.PI * radius;
     }
 
     @Override
     public void draw(GraphicsContext gc) {
-        gc.setFill(color);
+        gc.save();
+        double centerX = x + width  / 2;
+        double centerY = y + height / 2;
+        gc.translate(centerX, centerY);
+        gc.scale(2, 2);
+        gc.translate(-centerX, -centerY);
+        gc.setFill(Color.LIGHTBLUE);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
         gc.strokeOval(x, y, width, height);
         gc.fillOval(x, y, width, height);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(1);
+        if (inputIsDiameter) {
+            double leftX  = x;
+            double rightX = x + width;
+            gc.strokeLine(leftX, centerY, rightX, centerY);
+        } else {
+            double rightX = x + width;
+            gc.strokeLine(centerX, centerY, rightX, centerY);
+        }
+        double dotSize = 2;
+        gc.setFill(Color.BLACK);
+        gc.fillOval(centerX - dotSize / 2, centerY - dotSize / 2, dotSize, dotSize);
+        gc.restore();
     }
-
+    public String getInputText() {
+        String unit = inputIsDiameter ? "Diameter" : "Radius";
+        return String.format("%s = %.1f units", unit, inputValue);
+    }
     public String getAreaFormula() {
         return "Area = πr²";
     }
@@ -38,11 +65,11 @@ public class Circle extends Shape2D {
     }
 
     public String getAreaFormulaWithValues() {
-        return String.format("Area = π × %.1f² = %.1f", radius, calculateArea());
+        return String.format("Area = π × %.1f² = %.2f", radius, calculateArea());
     }
 
     public String getCircumferenceFormulaWithValues() {
-        return String.format("Circumference = 2π × %.1f = %.1f", radius, calculateCircumference());
+        return String.format("Circumference = 2π × %.1f = %.2f", radius, calculateCircumference());
     }
 
     public double getRadius() {
@@ -54,8 +81,6 @@ public class Circle extends Shape2D {
         this.width = radius * 2;
         this.height = radius * 2;
     }
-
-    // 新增circle的copy
     @Override
     public Circle copy() {
         Circle newCircle = new Circle(this.radius);
