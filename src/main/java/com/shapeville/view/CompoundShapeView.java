ea366cdbc9057aa8e53e9a7a78a269ba330a79ab
energyheng <3554643758@qq.com>
2025-05-17 22:21:52 +0800

    完成第九个图形的绘画

--- src/main/java/com/shapeville/view/CompoundShapeView.java
+++ src/main/java/com/shapeville/view/CompoundShapeView.java
@@ -223,7 +223,11 @@ public class CompoundShapeView extends VBox {
             // TODO: 添加图8的基本形状创建代码
         } else if (index == 8) {
             // 图9：楼梯形状（两个矩形） - 创建基本形状
-            // TODO: 添加图9的基本形状创建代码
+            // 根据图片，左侧矩形长11，宽10，右侧矩形长宽都为8
+            Rectangle rectLeft = new Rectangle(10 * scaleFactor, 11 * scaleFactor); // 左侧矩形 (宽x高)
+            Rectangle rectRight = new Rectangle(8 * scaleFactor, 8 * scaleFactor); // 右侧矩形 (宽x高)
+            currentShapes.add(rectLeft);
+            currentShapes.add(rectRight);
         }
         else {
             // 对于其他图形，可以暂时添加一个默认形状或者留空
@@ -304,7 +308,7 @@ public class CompoundShapeView extends VBox {
 
         } else if (currentShapeIndex == 1) {
              // 图2的特殊绘制：统一颜色，只绘制外轮廓
-            gc.setFill(Color.LIGHTGREEN); // 统一设置为浅绿色
+            gc.setFill(Color.LIGHTBLUE); // 统一设置为浅蓝色
 
             Rectangle rect1 = (Rectangle) currentShapes.get(0); // 左上角矩形
             Rectangle rect2 = (Rectangle) currentShapes.get(1); // 下方矩形
@@ -426,16 +430,54 @@ public class CompoundShapeView extends VBox {
             // TODO: 添加图8的填充颜色和外轮廓绘制代码
         } else if (currentShapeIndex == 8) {
              // 图9的特殊绘制：统一颜色，只绘制外轮廓
-            // TODO: 添加图9的填充颜色和外轮廓绘制代码
-        }
-        else {
-            // 其他图形的通用绘制逻辑（作为未实现图形的默认显示）
-            for (int i = 0; i < currentShapes.size(); i++) {
-                Shape2D shape = currentShapes.get(i);
-                // gc.setFill(colors[i % colors.length]); // 通用颜色，可根据需要调整
-                gc.setFill(Color.GRAY); // 暂时用灰色区分未实现的图形
-                shape.draw(gc); // 使用Shape自带的draw方法进行填充和边框绘制
-            }
+            gc.setFill(Color.LIGHTBLUE); // 统一设置为浅蓝色
+
+            Rectangle rectLeft = (Rectangle) currentShapes.get(0); // 左侧矩形
+            Rectangle rectRight = (Rectangle) currentShapes.get(1); // 右侧矩形
+
+            double rectLeftX = rectLeft.getX();
+            double rectLeftY = rectLeft.getY();
+            double rectLeftWidth = rectLeft.getWidth();
+            double rectLeftHeight = rectLeft.getHeight();
+
+            double rectRightX = rectRight.getX();
+            double rectRightY = rectRight.getY();
+            double rectRightWidth = rectRight.getWidth();
+            double rectRightHeight = rectRight.getHeight();
+
+            // 填充整个复合图形
+            double[] xPointsFill = {
+                rectLeftX,                                         // 左上
+                rectLeftX + rectLeftWidth,                        // 左上到右上（连接）
+                rectLeftX + rectLeftWidth,                        // 转折点，左矩形右上
+                rectRightX + rectRightWidth,                      // 右矩形右上
+                rectRightX + rectRightWidth,                      // 右矩形右下
+                rectLeftX + rectLeftWidth,                        // 左矩形右下
+                rectLeftX,                                         // 左矩形左下
+            };
+            
+            double[] yPointsFill = {
+                rectLeftY,                                         // 左上
+                rectLeftY,                                         // 左矩形上边
+                rectRightY,                                        // 连接点（右矩形顶部）
+                rectRightY,                                        // 右矩形右上
+                rectRightY + rectRightHeight,                     // 右矩形右下
+                rectLeftY + rectLeftHeight,                       // 左矩形右下
+                rectLeftY + rectLeftHeight                        // 左下
+            };
+            gc.fillPolygon(xPointsFill, yPointsFill, xPointsFill.length);
+
+            // 绘制外轮廓
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(2.0);
+
+            // 绘制外围六条边
+            gc.strokeLine(rectLeftX, rectLeftY, rectLeftX + rectLeftWidth, rectLeftY); // 左上横边
+            gc.strokeLine(rectLeftX + rectLeftWidth, rectLeftY + rectLeftHeight, rectRightX + rectRightWidth, rectLeftY + rectLeftHeight); // 连接两矩形的横边
+            gc.strokeLine(rectRightX + rectRightWidth, rectLeftY + rectLeftHeight, rectRightX + rectRightWidth, rectRightY + rectRightHeight); // 右下竖边
+            gc.strokeLine(rectRightX + rectRightWidth, rectRightY + rectRightHeight, rectLeftX, rectRightY + rectRightHeight); // 下横边
+            gc.strokeLine(rectLeftX, rectRightY + rectRightHeight, rectLeftX, rectLeftY); // 左侧竖边
+
         }
 
         addDimensionLabels();
@@ -538,7 +580,23 @@ public class CompoundShapeView extends VBox {
             // TODO: 添加图8的基本形状定位代码
         } else if (currentShapeIndex == 8) {
             // 图9：楼梯形状 - 定位基本形状
-            // TODO: 添加图9的基本形状定位代码
+            // 根据图片结构，左侧矩形宽10高11，右侧矩形宽8高8
+            // 整体宽度为 10 + 8 = 18
+            // 整体高度为 11
+            double totalWidth = (10 + 8) * 10.0;
+            double totalHeight = 11 * 10.0;
+
+            // 计算整体起始位置，使其居中
+            double startX = centerX - totalWidth / 2;
+            double startY = centerY - totalHeight / 2;
+
+            // 设置左侧矩形位置 (左上角)
+            Rectangle rectLeft = (Rectangle) currentShapes.get(0);
+            rectLeft.setPosition(startX, startY);
+
+            // 设置右侧矩形位置 (右侧上方)
+            Rectangle rectRight = (Rectangle) currentShapes.get(1);
+            rectRight.setPosition(startX + rectLeft.getWidth(), startY + (rectLeft.getHeight() - rectRight.getHeight())); // 在左侧矩形右侧，底部对齐
         }
         else {
             // 其他图形的定位逻辑，暂时留空或默认处理
@@ -795,7 +853,49 @@ public class CompoundShapeView extends VBox {
             // TODO: 添加图8的尺寸标注代码
         } else if (currentShapeIndex == 8) {
              // 图9：楼梯形状 - 尺寸标注
-            // TODO: 添加图9的尺寸标注代码
+            Rectangle rectLeft = (Rectangle) currentShapes.get(0); // 左侧矩形
+            Rectangle rectRight = (Rectangle) currentShapes.get(1); // 右侧矩形
+
+            double rectLeftX = rectLeft.getX();
+            double rectLeftY = rectLeft.getY();
+            double rectLeftWidth = rectLeft.getWidth();
+            double rectLeftHeight = rectLeft.getHeight();
+
+            double rectRightX = rectRight.getX();
+            double rectRightY = rectRight.getY();
+            double rectRightWidth = rectRight.getWidth();
+            double rectRightHeight = rectRight.getHeight();
+
+            // 标注左侧高 11 cm
+            gc.fillText(String.format("%.0f cm", 11.0), rectLeftX - 30, rectLeftY + rectLeftHeight/2);
+            gc.strokeLine(rectLeftX - 5, rectLeftY, rectLeftX - 5, rectLeftY + rectLeftHeight);
+            gc.strokeLine(rectLeftX - 8, rectLeftY, rectLeftX - 2, rectLeftY);
+            gc.strokeLine(rectLeftX - 8, rectLeftY + rectLeftHeight, rectLeftX - 2, rectLeftY + rectLeftHeight);
+
+            // 标注底部总宽 18 cm
+            gc.fillText(String.format("%.0f cm", 18.0), rectLeftX + (rectLeftWidth + rectRightWidth)/2 - 15, rectLeftY + rectLeftHeight + 15);
+            gc.strokeLine(rectLeftX, rectLeftY + rectLeftHeight + 5, rectLeftX + rectLeftWidth + rectRightWidth, rectLeftY + rectLeftHeight + 5);
+            gc.strokeLine(rectLeftX, rectLeftY + rectLeftHeight + 2, rectLeftX, rectLeftY + rectLeftHeight + 8);
+            gc.strokeLine(rectLeftX + rectLeftWidth + rectRightWidth, rectLeftY + rectLeftHeight + 2, rectLeftX + rectLeftWidth + rectRightWidth, rectLeftY + rectLeftHeight + 8);
+
+            // 标注左侧顶部宽 10 cm
+            gc.fillText(String.format("%.0f cm", 10.0), rectLeftX + rectLeftWidth/2 - 15, rectLeftY - 10);
+            gc.strokeLine(rectLeftX, rectLeftY - 5, rectLeftX + rectLeftWidth, rectLeftY - 5);
+            gc.strokeLine(rectLeftX, rectLeftY - 8, rectLeftX, rectLeftY - 2);
+            gc.strokeLine(rectLeftX + rectLeftWidth, rectLeftY - 8, rectLeftX + rectLeftWidth, rectLeftY - 2);
+
+            // 标注右侧高 8 cm
+            gc.fillText(String.format("%.0f cm", 8.0), rectRightX + rectRightWidth + 10, rectRightY + rectRightHeight/2);
+            gc.strokeLine(rectRightX + rectRightWidth + 5, rectRightY, rectRightX + rectRightWidth + 5, rectRightY + rectRightHeight);
+            gc.strokeLine(rectRightX + rectRightWidth + 8, rectRightY, rectRightX + rectRightWidth + 2, rectRightY);
+            gc.strokeLine(rectRightX + rectRightWidth + 8, rectRightY + rectRightHeight, rectRightX + rectRightWidth + 2, rectRightY + rectRightHeight);
+
+             // 标注右侧宽 8 cm
+            gc.fillText(String.format("%.0f cm", 8.0), rectRightX + rectRightWidth/2 - 15, rectRightY - 10);
+            gc.strokeLine(rectRightX, rectRightY - 5, rectRightX + rectRightWidth, rectRightY - 5);
+            gc.strokeLine(rectRightX, rectRightY - 8, rectRightX, rectRightY - 2);
+            gc.strokeLine(rectRightX + rectRightWidth, rectRightY - 8, rectRightX + rectRightWidth, rectRightY - 2);
+
         }
         // 其他图形的尺寸标注逻辑，待实现
     }
